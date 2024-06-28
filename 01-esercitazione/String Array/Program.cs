9813864cef04346b6ffbd6aef2ee6a41582c2497
hasrial <griolis24@gmail.com>
2024-06-28 10:39:25 +0200

    creazione di vari sistemi array

---
+++ 01-esercitazione/String Array/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] nomi = new string[3];
+    nomi[0] = "Mario";
+    nomi[1] = "Luigi";
+    nomi[2] = "Giovanni";
+    // Console.WriteLine($"Ciao {nomi[0]}, {nomi[1]} e {nomi[2]}");
+    // stampo tramite for each
+    foreach (string nome in nomi)
+    {
+        Console.WriteLine ($"Ciao {nome}");
+    }
